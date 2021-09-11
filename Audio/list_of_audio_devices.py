1c7b810139d6ea31872bf928243b9bf1f2cd616d
chess <chessismylife95@gmail.com>
2021-09-11 23:24:19 +0530

    testing

--- Audio/list_of_audio_devices.py
+++ Audio/list_of_audio_devices.py
@@ -1,6 +1,9 @@
 import speech_recognition as s_r
+print('List of Audio devices')
 print(s_r.Microphone.list_microphone_names())
 
+
+#sample output
 '''
 ['Microsoft Sound Mapper - Input', 'External Microphone (Realtek(R)', 'Microphone (Realtek(R) Audio)',
  'Microsoft Sound Mapper - Output', 'Headphones (Realtek(R) Audio)', 'Speakers (Realtek(R) Audio)']
